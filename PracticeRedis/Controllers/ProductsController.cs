37abc5c90cca5cde1865b86e2d88e9d6e9d98718
berk <berk_baybas@hotmail.com>
2023-03-24 09:57:45 +0300

    Add project files.

---
+++ PracticeRedis/Controllers/ProductsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using System;
+
+namespace PracticeRedis.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private IDistributedCache _distributeCache;
+        public ProductsController(IDistributedCache distributedCache)
+        {
+            _distributeCache = distributedCache;
+        }
+
+        public IActionResult Index()
+        {
+            DistributedCacheEntryOptions cacheOptions = new DistributedCacheEntryOptions();
+            cacheOptions.AbsoluteExpiration = DateTime.Now.AddMinutes(1);
+            _distributeCache.Remove("name");
+            _distributeCache.SetString("name", "Berk");
+            string name = _distributeCache.GetString("name");
+
+            return View();
+        }
+    }
+}
